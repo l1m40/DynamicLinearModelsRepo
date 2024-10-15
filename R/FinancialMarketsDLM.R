efb5aa4fae082c318ccb3b708a9fc86a4f6bc84f
Marcos Lima <marcoslima@Marcoss-MacBook-Pro.local>
2024-10-15 11:14:06 -0300

    dlmFilterDF

--- R/FinancialMarketsDLM.R
+++ R/FinancialMarketsDLM.R
@@ -121,8 +121,8 @@ result_basic_df <- price_forecast_not_estimated(Y_df,80,build)
 result_mle_df   <- price_forecast_mle(Y_df,80,build)
 
 NULL %>% 
-  rbind(result_basic_df %>% summarise(error=mean((forecast/price)-1,na.rm=T)) %>% mutate(test="basic estimate")) %>% 
-  rbind(result_mle_df   %>% summarise(error=mean((forecast/price)-1,na.rm=T)) %>% mutate(test="maximum likelihood"))
+  rbind(result_basic_df %>% summarise(error=mean(abs((forecast/price)-1),na.rm=T)) %>% mutate(test="basic estimate")) %>% 
+  rbind(result_mle_df   %>% summarise(error=mean(abs((forecast/price)-1),na.rm=T)) %>% mutate(test="maximum likelihood"))
 
 result_mle_df %>% ggplot(aes(x=date))+#geom_line(aes(y=dW1))
   geom_ribbon(data=result_basic_df,aes(ymax=(forecast+sd_residuals*qnorm(0.95)),ymin=(forecast-sd_residuals*qnorm(0.95))),alpha=.1,fill="gray")+
@@ -151,7 +151,7 @@ Y_df <- Y_df %>% arrange(date) %>% mutate(log_returns=log(price/lag(price,5))) %
 build <- function(params) { return(dlmModPoly(dV=exp(params[1]),dW=c(exp(params[2:3])))) }
 result_basic_df <- price_forecast_not_estimated(Y_df,80,build)
 result_mle_1_df <- price_forecast_mle(Y_df,80,build)
-result_mle_df   <- price_forecast_mle(Y_df %>% mutate(price=(price+high+low)/3),80,build)
+result_mle_df   <- price_forecast_mle(Y_df %>% filter(open>0) %>% mutate(price=(price+high+low)/3),80,build)
 
 NULL %>% 
   rbind(result_basic_df %>% mutate(error=abs((forecast/price)-1)) %>% mutate(err_mean=mean(error,na.rm=T),err_max=max(error,na.rm=T)) %>% mutate(test="  not estimated")) %>% 
@@ -170,6 +170,45 @@ result_mle_df %>% ggplot(aes(x=date))+#geom_line(aes(y=dW1))
   #geom_point(data=result_df %>% mutate(forecast=ifelse(NVR1<100,forecast,NA)),aes(y=forecast),size=3,color="green3",alpha=.5)+
   theme_minimal()
 
+
+
+
+
+
+
+
+
+
+
+
+
+# dlmFilterDF ##############################################
+plot_smoothed_dlmFilterDF <- function(df){
+  df %>% mutate(out_ci=(price>forecast+ci | price<forecast-ci)) %>% 
+    ggplot(aes(x=date))+
+    # geom_ribbon(aes(ymax=(forecast+sd_residuals*qnorm(0.95)),ymin=(forecast-sd_residuals*qnorm(0.95))),alpha=.1,fill="cyan3")+
+    geom_ribbon(aes(ymax=(forecast+ci),ymin=(forecast-ci)),alpha=.1,fill="cyan3")+
+    geom_line(data=Y_df,aes(y=price),color="gray")+
+    geom_line(aes(y=price   ),color="black")+geom_point(aes(y=price    ,color=out_ci),size=.5)+scale_color_manual(values=c("TRUE"="red","FALSE"="black"))+
+    geom_line(aes(y=forecast),color="cyan3")+geom_point(aes(y=forecast),color="cyan3",size=.5)+
+    theme_minimal()
+}
+Y_df <- tq_get("ITUB4.SA",from=today()-300,to=today()+1,get="stock.prices") %>% mutate(price=adjusted,asset=str_remove(symbol,".SA")) %>% mutate(adjust_gap=close-adjusted,open=open-adjust_gap,high=high-adjust_gap,low=low-adjust_gap)
+Y_df <- Y_df %>% arrange(date) %>% mutate(log_returns=log(price/lag(price,5))) %>% select(date,open,high,low,price,log_returns) %>% mutate(price_mean_2=rollapply(price,list(-seq(1:2)),mean,fill=NA)) %>% tail(300) %>% mutate(row_no=row_number())
+
+result_dlmFilterDF_df         <- price_forecast_dlmFilterDF(Y_df,80)
+result_dlmFilterDF_typical_df <- price_forecast_dlmFilterDF(Y_df %>% filter(open>0) %>% mutate(price=(price+high+low)/3),80)
+
+NULL %>% 
+  rbind(result_dlmFilterDF_df         %>% select(forecast,price) %>% mutate(error=abs((forecast/price)-1)) %>% mutate(err_mean=mean(error,na.rm=T),err_max=max(error,na.rm=T)) %>% mutate(test="dlmFilterDF")) %>% 
+  rbind(result_dlmFilterDF_typical_df %>% select(forecast,price) %>% mutate(error=abs((forecast/price)-1)) %>% mutate(err_mean=mean(error,na.rm=T),err_max=max(error,na.rm=T)) %>% mutate(test="dlmFilterDF typical price")) %>% 
+  ggplot(aes(y=test))+ylab("Model")+xlab("Forecast Error Average")+geom_boxplot(aes(x=error))
+
+grid.arrange(
+  result_dlmFilterDF_df         %>% plot_smoothed_dlmFilterDF(),
+  result_dlmFilterDF_typical_df %>% plot_smoothed_dlmFilterDF(),
+  ncol=1
+)
 
 
 
